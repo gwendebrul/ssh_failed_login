fa6e73a9900f5c0f844b247c6b58d6920c6aa290
Gwen De Brul <gwen@debrul.be>
2019-01-30 13:53:53 +0100

    Refactored for using messenger instead of email alerts

--- ssh_failed_login.sh
+++ ssh_failed_login.sh
@@ -2,20 +2,11 @@
 
 source ./.env
 
-ATTEMPTS=$(grep -a -E 'invalid|REFUSED' /var/log/auth.log|grep -a -E "$(date +'%B %e')")
+ATTEMPTS=$(grep -a -E 'invalid|REFUSED' /var/log/auth.log|grep -a -E "$(date +'%e')"|grep -a -E "preauth")
 
 ATTEMPTS_COUNT=$(echo "$ATTEMPTS"|wc -l)
 
-MESSAGE=$(echo -e "on $(date +'%B %e %Y') there were $ATTEMPTS_COUNT failed login attempts on $(hostname)\n\n$ATTEMPTS")
+MESSAGE=$(echo -e "on $(date +'%B %e %Y') there were $ATTEMPTS_COUNT failed login attempts on $(hostname)")
 
-curl -s --user "api:$MAILGUN_API_KEY" \
-  https://api.mailgun.net/v3/"$MAILGUN_DOMAIN"/messages \
-  -F from="$MAILGUN_SENDER_NAME <$MAILGUN_SENDER_EMAIL>" \
-  -F to=gwen@debrul.be \
-  -F subject="Daily Security Report on $(hostname)" \
-  -F text="$MESSAGE"
-
-
-echo
-
-
+curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"$MESSAGE\"}" $IFTTT_FAILED_SSH
+#echo "{\"value1\":\"$MESSAGE\"}" $IFTTT_FAILED_SSH
